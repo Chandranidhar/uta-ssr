46bcf57236c61f1b9185e253663fa828fa133e50
koushikbiswas <biswakoushik.influxiq@gmail.com>
2019-09-06 11:53:48 +0530

    koushik

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,10 +8,10 @@ import {MetaService } from '@ngx-meta/core';
 export class AboutComponent implements OnInit {
 
   constructor(public readonly metaService:MetaService) {
-    this.metaService.setTitle("Universal Tech Associates: About UTA");
-    this.metaService.setTag('description',"About UTA: Universal Tech Associates is a consortium of high-power global partnerships working with emerging technologies in the current global marketplace.");
-    this.metaService.setTag('og:description',"About UTA:Universal Tech Associates is a consortium of high-power global partnerships working with emerging technologies in the current global marketplace.");
-    this.metaService.setTag('og:title',"Universal Tech Associates: About UTA");
+    this.metaService.setTitle("Universal Tech Associates: About Universal Tech Associates");
+    this.metaService.setTag('description',"About Universal Tech Associates: Universal Tech Associates is a consortium of high-power global partnerships working with emerging technologies in the current global marketplace.");
+    this.metaService.setTag('og:description',"About Universal Tech Associates:Universal Tech Associates is a consortium of high-power global partnerships working with emerging technologies in the current global marketplace.");
+    this.metaService.setTag('og:title',"Universal Tech Associates: About Universal Tech Associates");
     this.metaService.setTag('og:image',"http://www.universaltechassociates.com/assets/images/logo.png");
     this.metaService.setTag('og:type',"website");
     // this.metaService.setTag('og:image',"Universal Tech Associates");
